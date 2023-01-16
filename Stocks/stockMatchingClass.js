d5bd1103a5a866ea3c156f830bd617004fff94e2
Sami <52921619+sbagh@users.noreply.github.com>
2023-01-16 16:11:50 -0500

    highest buy highest sell order matching

--- Stocks/stockMatchingClass.js
+++ Stocks/stockMatchingClass.js
@@ -1,3 +1,60 @@
-class StockMatching {
-    
+//matches highest buy order to highest sell order
+class stockMatchingSystem {
+   //buy and sellOrders are arrays of objects containing trade info such as buyer/seller, qty, price, time, type, status..etc
+   //e.g:
+   //buyOrders = [{buyerID: "11123", symbol: "TSLA", quantity: 10, price:100, time: 18:00 EST, type:buy, status:open}]
+
+   constructor() {
+      (this.buyOrders = []), (this.sellOrders = []);
+   }
+
+   addBuyOrder(order) {
+      this.buyOrders.push(order);
+   }
+
+   addSellOrder(order) {
+      this.sellOrders.push(order);
+   }
+
+   matchOrders() {
+      //first sort buy and sell orders in descending order to get highest value of each
+      this.buyOrders.sort((a, b) => b.price - a.price);
+      this.sellOrders.sort((a, b) => b.price - a.price);
+
+      let matchedOrders = [];
+
+      let i = 0,
+         j = 0;
+      while (i < this.buyOrders.length && j < this.sellOrders.length) {
+         if (this.buyOrders[i].price >= this.sellOrders[j].price) {
+            let tradeQuanity = Math.min(
+               this.buyOrders[i].quantity,
+               this.sellOrders[j].quantity
+            );
+
+            this.buyOrders[i].quantity -= tradeQuanity;
+            this.sellOrders[j].quantity += tradeQuanity;
+
+            matchedOrders.push({
+               buyer: this.buyOrders[i].buyer,
+               seller: this.sellOrders[j].seller,
+               price: this.sellOrders[j].price,
+               quantity: tradeQuanity,
+            });
+
+            if (this.buyOrders[i] === 0) {
+               i++;
+            } else {
+               j++;
+            }
+         } else {
+            j++;
+         }
+      }
+      return matchedOrders;
+   }
+}
+
+module.exports = {
+    stockMatchingSystem
 }
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const { Portfolio } = require(`./Stocks/stockPortfolio`);
+const { stockMatchingSystem } = require("./Stocks/stockMatchingClass");
 
-const myPortfolio = new Portfolio(10000, { TSLA: 200, AMD: 100 });
+// const myPortfolio = new Portfolio(10000, { TSLA: 200, AMD: 100 });
 
-console.log(myPortfolio);
+// console.log(myPortfolio);
 
-// immediatly invoked function expression in async
-(async () => {
-   await myPortfolio.currentValue().then((val) => console.log(val));
-   let sellOrder1 = myPortfolio.sellStock("TSLA", 200, 100);
-   console.log(sellOrder1);
+// // immediatly invoked function expression in async
+// (async () => {
+//    await myPortfolio.currentValue().then((val) => console.log(val));
+//    let sellOrder1 = myPortfolio.sellStock("TSLA", 200, 100);
+//    console.log(sellOrder1);
 
-   await myPortfolio.currentValue().then((val) => console.log(val));
-   let sellOrder2 = myPortfolio.sellStock("TSLA", 200, 100);
-   console.log(sellOrder2);
+//    await myPortfolio.currentValue().then((val) => console.log(val));
+//    let sellOrder2 = myPortfolio.sellStock("TSLA", 200, 100);
+//    console.log(sellOrder2);
 
-   let buyOrder1 = myPortfolio.buyStock("TSLA", 100, 100);
-   console.log(buyOrder1);
-   await myPortfolio.currentValue().then((val) => console.log(val));
-})();
+//    let buyOrder1 = myPortfolio.buyStock("TSLA", 100, 100);
+//    console.log(buyOrder1);
+//    await myPortfolio.currentValue().then((val) => console.log(val));
+// })();
+
+const myStockMatchingSystem = new stockMatchingSystem();
+
+myStockMatchingSystem.addBuyOrder({ buyer: "john", price: 10, quantity: 20 });
+myStockMatchingSystem.addSellOrder({ seller: "Ron", price: 9, quantity: 16 });
+
+let matchedOrders = myStockMatchingSystem.matchOrders();
+
+console.log(matchedOrders);
