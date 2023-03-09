6e182617251f38b95c4e4f6b08bf7c78e5ca21a0
Sami <52921619+sbagh@users.noreply.github.com>
2023-03-09 09:49:40 -0500

    Received matched order message to user portfolio microservice
    Received matched order message to user portfolio microservice:
    
    - created and executed a receive message function from the fan out exchange initiated by order matching microservice

--- microservices/servers/user_portfolio/index.js
+++ microservices/servers/user_portfolio/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 // require functions to send and receive messages to amqp/rabbitMQ queue
-const { receiveFromQue } = require("./rabbitMQ");
+const { receiveFromFanOutExchange } = require("./rabbitMQ");
 
 const app = express();
 app.use(cors());
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
-// receive messages from order matching queue using rabbitMQ/amqplib:
-const matchedOrdersQueue = "matchedOrdersQueue";
+//recieve matched order from this fan out exchange and queue
+const matchedOrdersExchange = "matchedOrdersExchange";
+const matchedOrdersQueue = "matchedOrdersUserPortfolioQueue";
 
 const userPortfolioPORT = 4001;
 
@@ -36,29 +37,32 @@ app.get("/getUserStockHoldings", (req, res) => {
    });
 });
 
-// // receive matched order from order_matching microservice
-// const receiveMatchedOrder = async () => {
-//    const matchedOrder = await receiveFromQue(matchedOrdersQueue);
-//    console.log(
-//       `matched order received from ${matchedOrdersQueue} queue, order: `,
-//       matchedOrder
-//    );
+// receive matched order from order_matching microservice
+const receiveMatchedOrder = async () => {
+   const matchedOrder = await receiveFromFanOutExchange(
+      matchedOrdersExchange,
+      matchedOrdersQueue
+   );
+   console.log(
+      `matched order received from ${matchedOrdersQueue} queue, order: `,
+      matchedOrder
+   );
 
-//    // update user cash and stock holdings after matched order is received
-//    service.updateUserCashHoldingsAfterMatch(
-//       matchedOrder.buyerID,
-//       matchedOrder.sellerID,
-//       matchedOrder.price,
-//       matchedOrder.quantity
-//    );
-//    service.updateUserStockHoldingsAfterMatch(
-//       matchedOrder.buyerID,
-//       matchedOrder.sellerID,
-//       matchedOrder.ticker,
-//       matchedOrder.quantity
-//    );
-// };
-// setInterval(receiveMatchedOrder, 1000);
+   // // update user cash and stock holdings after matched order is received
+   // service.updateUserCashHoldingsAfterMatch(
+   //    matchedOrder.buyerID,
+   //    matchedOrder.sellerID,
+   //    matchedOrder.price,
+   //    matchedOrder.quantity
+   // );
+   // service.updateUserStockHoldingsAfterMatch(
+   //    matchedOrder.buyerID,
+   //    matchedOrder.sellerID,
+   //    matchedOrder.ticker,
+   //    matchedOrder.quantity
+   // );
+};
+setInterval(receiveMatchedOrder, 1000);
 
 // // update buy and seller user portfolios after an order is matched, received from order matching microservice
 // app.put("/updateUserPortfolioAfterMatch", (req, res) => {
