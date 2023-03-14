053cbf8ccecc97af5b55a22c4cea8811e8cdacad
Sami <52921619+sbagh@users.noreply.github.com>
2023-03-14 10:57:44 -0400

    created user_accounts replica table in user portfolio microservice
    - created user_accounts replica table in user portfolio microservice
    - added account_status row in both tables; this will show if user is active, pending, deleted, suspended

--- microservices/servers/user_accounts/database/database.sql
+++ microservices/servers/user_accounts/database/database.sql
@@ -3,5 +3,6 @@ CREATE DATABASE user_accounts;
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   first_name TEXT,
-  last_name TEXT
+  last_name TEXT,
+  account_status VARCHAR(20) NOT NULL
 );
--- microservices/servers/user_portfolio/database/database.sql
+++ microservices/servers/user_portfolio/database/database.sql
@@ -2,14 +2,18 @@ CREATE DATABASE user_portfolio;
 
 CREATE TABLE cash_holdings (
   portfolio_id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES user_accounts_replica(user_id),
   cash NUMERIC NOT NULL
 );
 
 CREATE TABLE stock_holdings (
   portfolio_id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES user_accounts_replica(user_id),
   ticker TEXT NOT NULL,
   quantity INTEGER NOT NULL
 );
 
+CREATE TABLE user_accounts_replica (
+  user_id INTEGER NOT NULL,
+  account_status VARCHAR(20) NOT NULL
+)
